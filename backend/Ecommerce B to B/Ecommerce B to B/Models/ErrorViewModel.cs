347617dc9a3f186c6479033df22a1b8079cb9c3c
Ecommerce B to B <rushikeshdhavan26209@gmail.com>
2025-12-28 11:27:31 +0530

    backend templete is created

---
+++ backend/Ecommerce B to B/Ecommerce B to B/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ecommerce_B_to_B.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
